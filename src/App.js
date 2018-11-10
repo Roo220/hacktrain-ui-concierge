3c199e5a4aa75ddaa97a2a8a9de9d0473194038c
Richard Lindsay <rich.lindsay@live.co.uk>
2018-11-10 09:19:13 +0100

    Added cookie

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
+import { getCookie } from './utils';
 // import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props)
+    const usernameCookie = getCookie("conciergeUsername")
+    console.log("username: ", usernameCookie)
     this.state = {
       error: null,
       loading: false,
-      username: "",
+      username: usernameCookie || "",
       name: "",
       seatNumber: ""
     }
+    this.updateInput = this.updateInput.bind(this)
+    this.setCookie = this.setCookie.bind(this)
   }
 
-  setCookie(data) {
+  setCookie(event) {
+    console.log("setCookie()")
+    event.preventDefault()
     console.log("Setting cookie and loading chat...")
-    console.log(data)
+    const username = this.state.name + this.state.seatNumber
+    document.cookie = "conciergeUsername=" + username
+    this.setState({username: username})
+  }
 
+  updateInput(event) {
+    const target = event.target;
+    console.log("Target: ", target)
+    const key = target.name
+    const value = target.value
+    console.log("Key: ", key)
+    console.log("Value: ", value)
+
+    this.setState({[key]: value});
   }
 
   render() {
@@ -33,21 +52,21 @@ class App extends Component {
         </header>
           { username ?
             <div>
-
+              Welcome
             </div>
           :
             <div>
               <p>Enter your name and seat number to begin:</p>
 
-              <form onSubmit={() => this.setCookie(this)}>
+              <form onSubmit={this.setCookie}>
                 <p>
                   <label>Name: </label>
-                  <input id="name" type="text" value={name} />
+                  <input id="name" type="text" name="name" value={this.state.name} onChange={this.updateInput}/>
                 </p>
                 <p><label>Seat Number: </label>
-                  <input id="seatNumber" type="text" value={seatNumber} />
+                  <input id="seatNumber" type="text" name="seatNumber" value={this.state.seatNumber} onChange={this.updateInput}/>
                 </p>
-                <input type="submit" />
+                <input type="submit" value="Submit" />
               </form>
             </div>
           }
