f96bc99336ea23e94f8520b859811072dc243a42
sxs1476 <stanicisebastian@yahoo.com>
2018-11-11 06:20:08 +0000

    Added two new functionalities

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import globalIcon from './icon/web.svg';
 import conductorIcon from './icon/user.svg'; 
 import carriageIcon from './icon/user-group.svg'; 
 import cartIcon from './icon/cart.svg'; 
+import feedbackIcon from './icon/lightbulb.svg'; 
+import helpIcon from './icon/question.svg'; 
 
 class App extends Component {
   constructor(props) {
@@ -68,18 +70,68 @@ class App extends Component {
         </header>
           { username ?
             <div>
-            { this.state.room != "" ?
+            { this.state.room === "help" ?
+              <div>
+                <h1> How to use this app? </h1> 
+                <p> Click on one of the given rooms to access them: </p>
+                <ul>
+                  <li> <b> Global Room </b> is used for every person in this train. </li>
+                  <li> <b> Carriage Room </b> is used for every person in the carriage you are in. </li>
+                  <li> <b> Concierge Room </b> is used for any person that wishes to use the chatbot. </li>
+                  <li> <b> Trolley Service Room </b> is used for anyone that wants to order food. </li>
+                </ul>
+                <p> Click on the back icon to come back to the main selection menu. </p> 
+                <p> More help to be added soon! </p> 
+                <input type="submit" value="Return to room selection menu!" onClick={() => {window.location.reload();}}/>
+              </div>
+              : this.state.room === "feedback" ?
+              <div>
+                <h1> We want your feedback over our app! </h1> 
+                <p> Do you think there are any good ways we can improve the app? Or are there any faults or problems that you have found? </p>
+                <form> 
+                  <textarea placeholder="Write to us here.." rows="8" cols="100"/> <br />
+                  <input type="submit" value="Send!" onClick={() => {window.location.reload();}}/>
+                </form> 
+              </div>
+              : this.state.room != "" ?
               <div>
                 <Messages room={room} />
               </div>      
-              : 
+              :
               <div className="menu">
-                <center><h2> Select a room.. </h2></center>
-                <button className="mainButton" onClick={() => {this.setRoom("global")}}> <img src={globalIcon} /> <br /> Global </button>
-                <button className="mainButton" onClick={() => {this.setRoom("conductor")}} > <img src={conductorIcon} /> <br /> Conductor </button>
-                <br /> 
-                <button className="mainButton" onClick={() => {this.setRoom("carriage")}}> <img src={carriageIcon} /> <br /> Carriage </button>
-                <button className="mainButton" onClick={() => {this.setRoom("trolleyService")}}> <img src={cartIcon} /> <br /> TrolleyService </button>
+                <center>
+                <h2> Select a room.. </h2>
+                  <table>
+                   <tr>
+                     <td>
+                     
+                     </td>
+                     <td>
+                      <button className="mainButton" onClick={() => {this.setRoom("global")}}> <img src={globalIcon} /> <br /> Global </button>
+                     </td>
+                     <td>
+                      <button className="mainButton" onClick={() => {this.setRoom("conductor")}} > <img src={conductorIcon} /> <br /> Conductor </button>
+                     </td>
+                     <td><p align="left">
+                      <button className="smallButton" onClick={() => {this.setRoom("help")}} > <img src={helpIcon} /> <br /> Help </button>
+                     </p></td>
+                    </tr>
+                    <tr>
+                      <td valign="bottom"><p align="right">
+                        <button className="smallButton2" onClick={() => {this.setRoom("feedback")}} > <img src={feedbackIcon} /> <br /> Feedback </button>
+                      </p></td>
+                      <td>
+                        <button className="mainButton" onClick={() => {this.setRoom("carriage")}}> <img src={carriageIcon} /> <br /> Carriage </button>
+                      </td>
+                      <td>
+                        <button className="mainButton" onClick={() => {this.setRoom("trolleyService")}}> <img src={cartIcon} /> <br /> TrolleyService </button>
+                      </td>
+                      <td>
+                      
+                      </td>
+                    </tr>
+                  </table> 
+                </center>
               </div>
             }
             </div>
